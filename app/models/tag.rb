467552476f8b45485a2c884c848e8af743549b9b
Thanh <ctt.bk.hcmut2009@gmail.com>
2015-03-30 23:14:05 +0700

    [DeepBlog] Add content tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ActiveRecord::Base
+    scope :available, 
+        -> post_id { where("id not IN (select tags.id from tags join tag_posts where tags.id = tag_posts.tag_id and tag_posts.post_id = #{post_id})") if post_id.present? }
     validates_presence_of :name
     has_many :tagPosts
     has_many :posts, through: :tagPosts
